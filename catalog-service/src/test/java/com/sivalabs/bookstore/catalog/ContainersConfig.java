f04fe76ed232fd2e03a943a243b57cc1f7589766
BOURGEOIS Olivier <bourgeoisolivier@hotmail.fr>
2024-12-06 07:28:38 +0100

    docker-compose postgresql

--- catalog-service/src/test/java/com/sivalabs/bookstore/catalog/ContainersConfig.java
+++ catalog-service/src/test/java/com/sivalabs/bookstore/catalog/ContainersConfig.java
@@ -1,4 +1,4 @@
-package com.sivalabs.catalog_service;
+package com.sivalabs.bookstore.catalog;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
@@ -7,12 +7,10 @@ import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.utility.DockerImageName;
 
 @TestConfiguration(proxyBeanMethods = false)
-class TestcontainersConfiguration {
-
-	@Bean
-	@ServiceConnection
-	PostgreSQLContainer<?> postgresContainer() {
-		return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
-	}
-
+public class ContainersConfig {
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"));
+    }
 }

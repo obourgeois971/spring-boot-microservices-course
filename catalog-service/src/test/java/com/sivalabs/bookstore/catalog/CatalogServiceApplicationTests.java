d508df5859f337554027512681dd7fd262e24714
BOURGEOIS Olivier <bourgeoisolivier@hotmail.fr>
2024-12-06 10:36:42 +0100

    fix test

--- catalog-service/src/test/java/com/sivalabs/bookstore/catalog/CatalogServiceApplicationTests.java
+++ catalog-service/src/test/java/com/sivalabs/bookstore/catalog/CatalogServiceApplicationTests.java
@@ -1,8 +1,12 @@
 package com.sivalabs.bookstore.catalog;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
-class CatalogServiceApplicationTests extends AbstractIT {
+@SpringBootTest
+@Import(ContainersConfig.class)
+class CatalogServiceApplicationTests {
 
     @Test
     void contextLoads() {}
